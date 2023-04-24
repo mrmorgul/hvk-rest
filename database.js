6da47d9e5ca765dea87c409cb2b4770c22aeb241
Rene L. Jensen <rlj@morgul.dk>
2023-04-24 22:26:19 +0200

    modified:   database.js

--- database.js
+++ database.js
@@ -1,24 +1,38 @@
 const { MongoClient } = require('mongodb');
 
 //const uri = process.env.MONGO_URI;
-const url = 'mongodb://root:example@localhost:27017/test';
-const client = new MongoClient(url);
-let database;
+const uri = 'mongodb://root:example@localhost:27017/test';
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+});
+
+//let database;
 
 async function run() {
   try {
     await client.connect();
-    database = client.db('test');
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!")
+    //database = client.db('test');
     }
-    catch (err) {
-        console.error(err);
+    finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
       }
     }
-
+    run().catch(console.dir);
+    
     const insert = async (request,callback) => {
         const insertDate = new Date().toISOString();
-        //const requests = database.collection('requests');
-        const result = await database.insertOne({request, requestDate: insertDate, lastUpdated: insertDate});
+        const myDB = client.db('test')
+        const myColl = myDB.collection('testcoll');
+        const result = await myColl.insertOne(request);
+        console.log('record added: ${result.insertedId}')
         callback(null, result);
     }
 
